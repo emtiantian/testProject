1f45979d3f5cf6f6bf2bf8976e02efdf39c67951
郝赫 <hao503106@163.com>
2016-04-22 15:05:21 +0800

    updata

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
-
+//引入组件
+var jshint = require('gulp-jshint');
+gulp.task("lint",function(){
+	gulp.src("./main/js/*.js").pipe(jshint()).pipe(jshint.reporter("default"));
+});
 gulp.task('default', function() {
   // 将你的默认的任务代码放在这
+  	gulp.run("lint");
  	console.log("helow word");
 });
